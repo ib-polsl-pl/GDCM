6d774cc7c1f9f57e59fabdbd8d12849a36b75dd2
Jacek Kawa <jacek.kawa@radpoint.pl>
2019-11-28 02:06:58 +0100

    Add a wrapper-builder to gdcmanon to show how to use certificates and new options in Python

---
+++ Applications/Python/gdcmanon/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Created on Mon Nov 25, 00:01:00 2019
+@author jacek.kawa@radpoint.pl
+'''
+
+import os
+import sys
+
+# sorry for that, import custom GDCM (LD_LIBRARY_PATH to import shared libs
+#sys.path.insert to import modules)
+sys.path.insert(1, os.getenv('LD_LIBRARY_PATH'))
+
+import gdcm
+
+gdcm.Trace.DebugOn()
+gdcm.Trace.WarningOn()
+gdcm.Trace.ErrorOn()
+
+
+def setup():
+    gg = gdcm.Global.GetInstance()
+    pth = os.getenv('GDCM_RESOURCES_PATH')
+    #pth = '/usr/share/gdcm-3.0/XML/'
+    if pth is not None:
+        gg.Prepend(pth)
+    gg.LoadResourcesFiles()
+
+
+setup()
